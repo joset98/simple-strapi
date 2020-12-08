401edcea8be5cf47d797d8bc2df563f88b6c4b9d
Joset98 <jtovar26455462@gmail.com>
2020-12-08 19:32:44 -0400

    initial struct

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9ec78dfd64bb9f2895efbb86af7e620e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5830e316-397c-4b8b-9132-62c733bf7450'
+};
